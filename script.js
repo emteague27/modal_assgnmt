6b369441667ec11880be4a8ba857a6d8a7d9293d
Erin <eteague27@gmail.com>
2015-12-12 18:30:25 -0500

    modal css done

--- script.js
+++ script.js
@@ -3,14 +3,12 @@ $(document).ready(function(){
 	$('body').on('mousedown', function(){
 		console.log('hello');
 		$('#modalContainer').show();
+		$('#contentBox').hide();
 		$('#modalContainer').on('click', function(){
 			$('#modalContainer').hide();
+			location.reload();
 		});
 	});
-
-
-
-
 
 
 	
